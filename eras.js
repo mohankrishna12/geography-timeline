b8429008f49e1e9ff216381f92ebd9e0ffce5e62
mohankrishna12 <mohankrishnammkr@gmail.com>
2025-12-14 06:58:23 +0530

    Add files via upload

---
+++ eras.js
@@ -0,0 +1,38 @@
+export const eras = [
+    {
+        id: 'pangea',
+        name: 'Pangea (Permian)',
+        year: '250 Million Years Ago',
+        mya: 250,
+        description: 'A supercontinent that existed during the late Paleozoic and early Mesozoic eras. It assembled from earlier continental units approximately 335 million years ago, and it began to break apart about 175 million years ago.',
+        texture: './src/assets/pangea.png',
+        color: '#8B5A2B'
+    },
+    {
+        id: 'jurassic',
+        name: 'Jurassic Period',
+        year: '150 Million Years Ago',
+        mya: 150,
+        description: 'Pangea began to break up into two supercontinents: Laurasia to the north and Gondwana to the south. The Atlantic Ocean opened up.',
+        texture: './src/assets/jurassic.png',
+        color: '#4A7023'
+    },
+    {
+        id: 'cretaceous',
+        name: 'Cretaceous Period',
+        year: '65 Million Years Ago',
+        mya: 65,
+        description: 'Continents continued to drift apart. India was moving north towards Asia. The Atlantic Ocean widened. Dinosaurs were dominant until the end of this period.',
+        texture: './src/assets/cretaceous.png',
+        color: '#5D8C3A'
+    },
+    {
+        id: 'modern',
+        name: 'Modern Day (Holocene)',
+        year: 'Present Day',
+        mya: 0,
+        description: 'The current configuration of continents. The Himalayas have formed from the collision of India and Asia. Ice sheets cover Antarctica and Greenland.',
+        texture: './src/assets/modern.png',
+        color: '#1E40AF'
+    }
+];
